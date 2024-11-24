2a5d2e573e82831b3e82967431f1900bff96ddf8
Andres-Sq <asu0090@gmail.com>
2024-11-24 14:07:44 -0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <> <Header /> </>
+    <> 
+    <Header /> 
+    <h1>TEXT</h1>
+    <Footer/> 
+    </>
   );
 }
 
